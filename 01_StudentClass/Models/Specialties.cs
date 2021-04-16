b741c8e6c7bf911713b27ef2dd7dcffc31166d5d
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-04-17 02:04:54 +0300

    add enumerations

---
+++ 01_StudentClass/Models/Specialties.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_StudentClass.Models
+{
+    enum Specialties
+    {
+        Anesthesiology,
+        Dermatology,
+        EmergencyMedicine,
+        FamilyMedicine,
+        InternalMedicine,
+        Cardiology,
+        Endocrinology,
+        Gastroenterology,
+        GeriatricMedicine,
+        Oncology,
+        Hematology,
+        Hospice,
+        PalliativeMedicine,
+        InfectiousDisease,
+        Nephrology,
+        PulmonaryDisease,
+        Rheumatology,
+        Neurology,
+        Ophthalmology,
+        Neurosurgery,
+        Accounting,
+        Finance,
+        DigitalApplications
+    }
+}
---
+++ 01_StudentClass/Models/Universities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_StudentClass.Models
+{
+    enum Universities
+    {
+        SofiaUniversity,
+        Cambridge,
+        Oxford,
+        Edinburgh,
+        UniversityCollegeLondon,
+        Aberdeen,
+        ImperialCollegeLondon,
+        QueenMary,
+        Newcastle,
+        Birmingham,
+        Manchester,
+        QueensBelfast,
+        Southampton,
+    }
+}
