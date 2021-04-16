945f4e4f7f8ef567b1d11ae341b81b4d3e0c623a
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-04-16 21:15:52 +0300

    add empty classes

---
+++ 01_StudentClass/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_StudentClass.Models;
+
+namespace _01_StudentClass
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
