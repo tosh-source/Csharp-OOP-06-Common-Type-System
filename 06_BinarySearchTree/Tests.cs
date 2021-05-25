e72b08111081b1970ebe1c4c186321f146539a0e
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-05-25 21:44:54 +0300

    add empty classes

---
+++ 06_BinarySearchTree/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_BinarySearchTree
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
