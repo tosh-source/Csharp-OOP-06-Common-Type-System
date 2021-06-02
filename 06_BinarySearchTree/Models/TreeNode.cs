878bf8fb3340818a36bcac36191ef59e4021d096
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-06-02 23:21:34 +0300

    override Equals and GetHashCode

--- 06_BinarySearchTree/Models/TreeNode.cs
+++ 06_BinarySearchTree/Models/TreeNode.cs
@@ -21,5 +21,34 @@ namespace BinarySearchTree
 
             return cloneTreeNode;
         }
+
+        public override bool Equals(object obj)
+        {
+            var node = obj as TreeNode;
+
+            if (node != null &&
+                Node == node.Node &&
+                EqualityComparer<TreeNode>.Default.Equals(this.Left, node.Left) &&
+                EqualityComparer<TreeNode>.Default.Equals(this.Right, node.Right))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 989432594;
+
+            hashCode = hashCode * -1521134295 + this.Node.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<TreeNode>.Default.GetHashCode(this.Left);
+            hashCode = hashCode * -1521134295 + EqualityComparer<TreeNode>.Default.GetHashCode(this.Right);
+
+            return hashCode;
+        }
     }
 }
